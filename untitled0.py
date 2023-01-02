03f9365c2633642ba90fd58f76ffa89173af8fd7
vihaan962 <82108747+vihaan962@users.noreply.github.com>
2023-01-02 17:59:09 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  7 18:38:33 2022
+
+@author: VIHAAN KATHURIA
+"""
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("HELLO I am .......")
+engine.runAndWait()
